0e9e5ad6926d24b50d198bd190f7bde93310db22
johannesvuorinen <johannes@frogmindgames.com>
2022-02-10 15:41:16 +0200

    Update SystemSettingsPlugin.kt

--- android/src/main/kotlin/de/florianweinaug/system_settings/SystemSettingsPlugin.kt
+++ android/src/main/kotlin/de/florianweinaug/system_settings/SystemSettingsPlugin.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.provider.Settings
-import androidx.annotation.NonNull;
+import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
